0f2d73f73f00e1a6218e542282ee3ff053d83634
Aayush <tripathiaayush249@gmail.com>
2025-06-11 17:37:29 +0530

    All O(1) Register Allocator

--- tundra/src/compiler/register.rs
+++ tundra/src/compiler/register.rs
@@ -2,38 +2,48 @@ use crate::bytecode::value::Value;
 
 /// A very simple register allocator using a free-list (LIFO),
 pub struct RegisterAllocator {
-    /// Stack of free register indices
-    free_registers:   Vec<usize>,
-    /// Most-recently allocated registers 
-    allocation_stack: Vec<usize>,
-    /// Current Value stored in each register slot
-    values:           Vec<Value>,
-    /// Whether each slot is currently in use
-    in_use:           Vec<bool>,
+    head: Option<usize>,          // start of the free list
+    next: [Option<usize>; 1024],  // next[i] = the register after i in the free list
+    prev: [Option<usize>; 1024],  // prev[i] = the register before i
+    in_use: [bool; 1024],         // whether i is currently allocated
+    values: [Value; 1024],        // store contents
+    allocation_stack: Vec<usize>, // stack of allocated registers
 }
 
 impl RegisterAllocator {
     /// Create 1024 registers, initially all free
     pub fn new() -> Self {
-        let mut free_registers = Vec::with_capacity(1024);
-        for i in (0..1024).rev() {
-            free_registers.push(i);
+        let mut next = [None; 1024];
+        let mut prev = [None; 1024];
+        for i in 0..1024 - 1 {
+            next[i] = Some(i + 1);
+            prev[i + 1] = Some(i);
         }
-        Self {
-            free_registers,
+        RegisterAllocator {
+            head: Some(0),
+            next,
+            prev,
+            in_use: [false; 1024],
+            values: core::array::from_fn(|_| Value::none()),
             allocation_stack: Vec::new(),
-            values:           vec![Value::none(); 1024],
-            in_use:           vec![false;    1024],
         }
     }
 
     /// Reserve a specific register so `allocate` won’t hand it out.
     pub fn reserve(&mut self, reg: usize) {
-        if let Some(pos) = self.free_registers.iter().position(|&r| r == reg) {
-            self.free_registers.remove(pos);
-        } else {
-            panic!("Tried to reserve register {} but it was not free", reg);
+        if self.in_use[reg] {
+            panic!("Cannot reserve an in-use register {}", reg);
         }
+        if let Some(prev_reg) = self.prev[reg] {
+            self.next[prev_reg] = self.next[reg];
+        } else {
+            self.head = self.next[reg];
+        }
+        if let Some(next_reg) = self.next[reg] {
+            self.prev[next_reg] = self.prev[reg];
+        }
+        self.next[reg] = None;
+        self.prev[reg] = None;
     }
 
     /// Read-only access to the Value in slot `reg`.
@@ -42,30 +52,38 @@ impl RegisterAllocator {
     }
 
     /// Allocate one register and associate it with `value`.
-    pub fn allocate(&mut self, value: Value) -> Result<usize, String> {
-        let reg = self.free_registers
-            .pop()
-            .ok_or_else(|| "Out of registers".to_string())?;
-        debug_assert!(!self.in_use[reg], "Allocating a register already in use");
-        self.in_use[reg]         = true;
-        self.values[reg]         = value;
+    pub fn allocate(&mut self, v: Value) -> Result<usize, String> {
+        let reg = self.head.ok_or("Out of registers")?;
+        self.head = self.next[reg];
+        if let Some(next_reg) = self.head {
+            self.prev[next_reg] = None;
+        }
+        self.in_use[reg] = true;
+        self.values[reg] = v;
+        self.next[reg] = None;
+        self.prev[reg] = None;
         self.allocation_stack.push(reg);
-
         Ok(reg)
     }
 
     /// Free a previously-allocated register.
     pub fn free(&mut self, reg: usize) {
-
-        if self.in_use[reg] {
-            self.in_use[reg] = false;
-            self.values[reg] = Value::none();             
-            self.free_registers.push(reg);                
-            if let Some(pos) = self.allocation_stack.iter().rposition(|&r| r == reg) {
-                self.allocation_stack.remove(pos);
-            }
+        if !self.in_use[reg] {
+            return;
         }
+        self.in_use[reg] = false;
+        self.values[reg] = Value::none();
+        if let Some(old_head) = self.head {
+            self.prev[old_head] = Some(reg);
+        }
+        self.next[reg] = self.head;
+        self.prev[reg] = None;
+        if let Some(pos) = self.allocation_stack.iter().rposition(|&r| r == reg) {
+            self.allocation_stack.remove(pos);
+        }
+        self.head = Some(reg);
     }
+
     /// Returns the most-recently allocated register
     pub fn last_allocated(&self) -> Option<usize> {
         self.allocation_stack.last().cloned()
